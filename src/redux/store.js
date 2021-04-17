cf29347d641392e8a8f052bcb78da5c4f358cc44
Aleksey Yesin <esinru@gmail.com>
2021-04-18 00:53:18 +0300

    Adds devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
 import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const initialState = {
     contacts: {
@@ -37,6 +38,6 @@ const reducer = (state = initialState, { type, payload }) => {
     }
 };
 
-const store = createStore(reducer);
+const store = createStore(reducer, composeWithDevTools());
 
 export default store;
